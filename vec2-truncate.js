b211e3dc8f525905a1f777139e7916235f0d6025
Mike Reinstein <mreinstein@sellpoints.com>
2020-09-07 13:54:06 -0700

    fix setLength invocation

--- vec2-truncate.js
+++ vec2-truncate.js
@@ -3,5 +3,5 @@ import { vec2, setLength } from './deps.js'
 
 export default function truncate (inp, maxLength) {
     if (vec2.length(inp) > maxLength)
-        setLength(inp, maxLength)
+        setLength(inp, inp, maxLength)
 }
